7404d42a5423302ef8cf6410498e3fbda97ce192
Mikita Buinavets <mikita.buinavets@arua.co>
2024-03-17 03:40:00 +0300

    module 1. task 1

--- src/main/java/com/epam/m1/exceptions/ParseIntegers.java
+++ src/main/java/com/epam/m1/exceptions/ParseIntegers.java
@@ -21,8 +21,13 @@ public class ParseIntegers {
         String justWords = "";
         while (words.hasNext()) {
             String next = words.next();
-            int number = Integer.parseInt(next);
-            // todo: complete it
+            try {
+                int number = Integer.parseInt(next);
+                sum += number;
+
+            } catch (NumberFormatException ex) {
+                justWords.append(" ").append(next);
+            }
         }
         System.out.println("Sum is " + sum);
         System.out.println("Just words:" + justWords);
